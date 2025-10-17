d6c52af63e80c41a85ff48314219534df5666dbb
Ngane Emmanuel <emmanuelngane06@gmail.com>
2025-10-16 18:11:07 -0700

    redirecting index.tsx to home page updating icons updating package json to remove preinstall legacy flag

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,4 @@
-import { Text, View } from "react-native";
-
+import { Redirect } from "expo-router";
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello DermaAI, ready to change the world?</Text>
-    </View>
-  );
-}
+   return <Redirect href="/(tabs)/home" />; 
+  }
